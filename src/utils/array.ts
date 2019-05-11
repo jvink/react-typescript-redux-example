5f5f2436f7fceead975b10f878e92e5ba4f3bcae
Jurian Vink <0950140@hr.nl>
2019-05-11 13:46:17 +0200

    added isarray help function

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,4 +1,5 @@
 import complement from 'ramda/src/complement';
 
+export const isArray = (possibleArray: any) => possibleArray instanceof Array ? true : false;
 export const isArrayEmpty = (array: any[]) => array.length === 0;
 export const isArrayNotEmpty = complement(isArrayEmpty);
